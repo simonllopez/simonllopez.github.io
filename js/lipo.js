30127c5134aa353885f6e0b88ade4a80dc5fdb6c
simon <simon.lopez@alisys.net>
2021-10-22 19:25:29 +0200

    nueva pag

---
+++ js/lipo.js
@@ -0,0 +1,11 @@
+a = 0;
+
+const calcular = () => {
+    a = a + 20;
+
+    document.getElementById(
+        "vtotal"
+    ).innerHTML = `Voltaje total: <b>${a}</b> V</div>`;
+
+    console.log("hi");
+};
