8a943fcec77a1cd8b436b328ec0e20f35adbdbf2
Simon <simonllopez@gmail.com>
2026-01-11 19:08:36 +0100

    newW

--- rosweb-dc-motor-simulator/settings.js
+++
@@ -1,6 +0,0 @@
-settingsButton = document.querySelector("#settings-button");
-settingsDiv = document.querySelector("#settings-div");
-
-settingsButton.addEventListener("click", (ev) => {
-  settingsDiv.classList.toggle("hide");
-});
